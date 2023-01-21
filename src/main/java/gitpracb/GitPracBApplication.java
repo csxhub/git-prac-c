389d231bdfb2bdbced7ef86329ed04373148691e
johnkhub <johnkatanga777@gmail.com>
2023-01-21 19:09:07 +0200

    ok ok

---
+++ src/main/java/gitpracb/GitPracBApplication.java
@@ -0,0 +1,13 @@
+package gitpracb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitPracBApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitPracBApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gitpracb/GitPracBApplicationTests.java
@@ -0,0 +1,13 @@
+package gitpracb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitPracBApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
