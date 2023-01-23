e7204428a4c56627b16966b911bd41893acd15d9
johnkhub <johnkatanga777@gmail.com>
2023-01-23 11:38:59 +0200

    second commit

--- src/main/java/gitpracb/GitPracBApplication.java
+++ src/main/java/gitpracb/GitPracBApplication.java
@@ -1,13 +1,22 @@
 package gitpracb;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GitPracBApplication {
+@Slf4j
+public class GitPracBApplication implements CommandLineRunner {
+
 
     public static void main(String[] args) {
+        log.info("CI/CD.....");
         SpringApplication.run(GitPracBApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("CICD in run------ ");
+    }
 }
--- src/test/java/gitpracb/GitPracBApplicationTests.java
+++ src/test/java/gitpracb/GitPracBApplicationTests.java
@@ -1,13 +1,19 @@
 package gitpracb;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 @SpringBootTest
+@Slf4j
 class GitPracBApplicationTests {
 
     @Test
     void contextLoads() {
+        log.info("TESRT passed.,.,., ");
+        assertEquals(true,true);
     }
 
 }
